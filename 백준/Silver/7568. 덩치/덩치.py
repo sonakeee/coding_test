0f632171d75c38f2511a98895853a3f25ee9cdc6
sonakeee <96439764+sonakeee@users.noreply.github.com>
2024-02-05 12:04:05 +0900

    [Silver V] Title: 덩치, Time: 68 ms, Memory: 35292 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,21 @@
+import sys
+from collections import defaultdict, deque
+import heapq
+
+input = sys.stdin.readline
+
+n = int(input())
+
+arr = []
+for i in range(n):
+    x, y = map(int, input().split())
+    arr.append((x, y))
+
+for i in arr:
+    rank = 1
+    for j in arr:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+
+    print(rank, end=' ')
+
